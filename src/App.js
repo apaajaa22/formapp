d508a9904e6774325fc5c8806dd16207f7f725fe
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-10-15 22:50:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import NextRegister from "./pages/NextRegister";
 import Confirmation from "./pages/Confirmation";
